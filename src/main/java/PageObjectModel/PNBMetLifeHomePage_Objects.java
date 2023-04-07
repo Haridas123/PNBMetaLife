c00c350f42504d6818e1e7301eb47a56c8502882
Haridas123 <tokalwadharidas@gmail.com>
2023-04-07 12:07:21 +0530

    leftcontainerItem test added

--- src/main/java/PageObjectModel/PNBMetLifeHomePage_Objects.java
+++ src/main/java/PageObjectModel/PNBMetLifeHomePage_Objects.java
@@ -7,9 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class PNBMetLifeHomePage_Objects {
-	WebDriver driver;
+	public WebDriver driver;
 	
 	By header_Navigation=By.xpath("//div[@class='header__navigation-container-menu-item-title font-eyebrow-1']");
+	By loginbtn=By.xpath("//label[@class='loginDropdown']");
+	By searchBtn= By.xpath("//div[@class='header__utilities']//div[@class='header__search-container']");
 	
 	public PNBMetLifeHomePage_Objects(WebDriver driver) {
 		 this.driver=driver;
@@ -19,6 +21,14 @@ public class PNBMetLifeHomePage_Objects {
 		
 		return driver.findElements(header_Navigation);
 		
+	}
+	
+	public WebElement loginAgent() {
+		return driver.findElement(loginbtn);
+	}
+	
+	public WebElement searchBtn() {
+		return driver.findElement(searchBtn);
 	}
 
 }
--- src/test/java/TestCases/PNBMetLIfeHomePage_TestCases.java
+++ src/test/java/TestCases/PNBMetLIfeHomePage_TestCases.java
@@ -3,24 +3,40 @@ package TestCases;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import PageObjectModel.CallYouBackObject;
 import PageObjectModel.PNBMetLifeHomePage_Objects;
 import Resources.BaseClass;
 
 public class PNBMetLIfeHomePage_TestCases extends BaseClass {
+	
+	
 	public List<WebElement> cuntryName;
 	public List<WebElement> headerItems;
-	public String name,name2;
+	public List<WebElement> agentList;
+	public List<WebElement> dotList;
+	public List<WebElement>headLines;
+	public List<WebElement> rightcontainerItems;
+	
+	public WebElement headText,showMore;
+	//public WebDriver driver;
+	public String name,name2,agentName,headLine,headLineText;
 	public PNBMetLifeHomePage_Objects pnbObjects;
-	
-	
 	 
+	
+	
+	
 	 
 	 @Test
-	public void headerItem() throws IOException, InterruptedException {
+	public void header() throws IOException, InterruptedException {
 		
 		driver=driverInitialization();
 		driver.get("https://www.pnbmetlife.com/");
@@ -28,15 +44,15 @@ public class PNBMetLIfeHomePage_TestCases extends BaseClass {
 		
 		pnbObjects= new PNBMetLifeHomePage_Objects(driver);
 		CallYouBackObject callObj= new CallYouBackObject(driver);
-		//Thread.sleep(7000);
+		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
 		callObj.toggleBtn().click();
 
 	}
 	 
-	@Test
+	//@Test
 	 public void headerItems() {
-
+		
 			headerItems=pnbObjects.headerItems();
 			
 			for(int j=0;j<headerItems.size();j++)
@@ -47,5 +63,86 @@ public class PNBMetLIfeHomePage_TestCases extends BaseClass {
 					action.moveToElement(headerItems.get(j)).perform();
 				
 			}
+			
 	 }
+	
+	@Test
+	public void loginAgents() throws InterruptedException {
+		
+		
+		pnbObjects= new PNBMetLifeHomePage_Objects(driver);
+		pnbObjects.loginAgent().click();
+		
+		agentList=driver.findElements(By.xpath("//ul[@class='dropdown-menu-inner']//li"));
+		dotList =driver.findElements(By.xpath("//ul[@class='slick-dots customSlickDots ']//li"));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		headLines=driver.findElements(By.xpath("//h1[@class='headline__item-text font-header-4 font-header-2-md']"));
+		
+		for(int a=0;a<agentList.size();a++)
+		{
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+			agentName = agentList.get(a).getText();
+			System.out.println("name of agents   "+agentName);
+			agentList.get(a).click();
+		}
+		
+		
+		pnbObjects.searchBtn().click();
+		for (int i=0;i<dotList.size();i++) {
+			dotList.get(i).click();
+			System.out.println("dots are clicked");
+		}
+		
+		for(int h=0;h<headLines.size();h++)
+		{
+			headLine=headLines.get(h).getText();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+			
+			System.out.println("headline   "+headLine+" reached ");
+		}
+		
+	}
+	
+	//@Test
+	public void leftAndRightContainer() throws IOException, InterruptedException {
+		
+		String nameText;
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		headText=driver.findElement(By.xpath("//div[@class='sorting-hat__headline-left font-header-2-md font-header-4-sm font-header-4']"));
+		
+		//rightcontainerItems=driver.findElements(By.xpath("//div[@class='col-12 col-sm-6 sorting-hat-menu-option sorting-hat-cta']"));
+		rightcontainerItems=driver.findElements(By.xpath("//div[@class='sorting-hat__itemlink']"));
+		showMore=driver.findElement(By.xpath("//div[@class='showmore-link']"));
+
+		headLineText=headText.getText().toString();
+		System.out.println("headText  "+headLineText);
+
+		 showMore.click();
+		 
+		 //JavascriptExecutor js = (JavascriptExecutor)driver;
+		 Actions actions = new Actions(driver);
+		for(int c=0;c<rightcontainerItems.size();c++) {
+
+			  nameText=rightcontainerItems.get(c).getText();
+			  System.out.println("text "+rightcontainerItems.get(c).getText());
+			  
+			  if(nameText.equalsIgnoreCase("Get a Callback"))
+			  {
+
+				  rightcontainerItems.get(c);  
+				  System.out.println("text "+rightcontainerItems.get(c).getText());
+			  }
+		  
+			  else  {
+				  
+				  actions.moveToElement(rightcontainerItems.get(c)).click().build().perform();
+				 // js.executeScript("arguments[0].click();", rightcontainerItems.get(c));
+				 // rightcontainerItems.get(c).click();
+				  System.out.println("Pay Premium Online clicked successfully "+rightcontainerItems.get(c).getText());
+			  }
+			  
+
+			}
+	}
 }
